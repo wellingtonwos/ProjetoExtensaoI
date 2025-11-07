4531f3d2bebe172bf4e24e2df3ef2e9cb32a420a
Gh0st4v0 <gustavo.135.andrade@gmail.com>
2025-11-06 22:57:18 -0300

    create basic structure for the backend, base spring app with dependencies

---
+++ Source/Server/SpringBootApp/src/main/java/com/example/SpringBootApp/JuniorPrimeBeefApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorPrimeBeefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorPrimeBeefApplication.class, args);
+	}
+
+}
